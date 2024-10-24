bd23112b103777cf358d6a15e6f63603ab796f02
qdyll <dylanchangjj@gmail.com>
2024-10-25 04:24:30 +0800

    Small changes

--- src/TeamDisplay.js
+++ src/TeamDisplay.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './TeamDisplay.css';
 
 const TeamDisplay = ({ teams, showDetails, calculateAverageRating }) => {
-
-  // Function to check if all players in a team have the neutral rating of 3
-  const allPlayersNeutral = (team) => {
-    return team.every(player => player.rating === 3);
-  };
-
   return (
     <div className="teams-container">
       {teams.map((team, index) => (
@@ -15,19 +9,18 @@ const TeamDisplay = ({ teams, showDetails, calculateAverageRating }) => {
           <h4>
             Team {index + 1}: {team.length} players
           </h4>
-
-          {/* Only show average rating if not all players have the neutral rating */}
-          {!allPlayersNeutral(team) && (
-            <h4>
-              Average Rating: {calculateAverageRating(team).toFixed(2)}
-            </h4>
-          )}
-          
+          <h4>
+            Average Rating: {calculateAverageRating(team).toFixed(2)}
+          </h4>
           <table className="table table-striped table-bordered">
             <thead>
               <tr>
-                <th>Name</th>
-                {showDetails && <th>Rating</th>}
+                {showDetails && (
+                  <>
+                    <th>Name</th>
+                    <th>Rating</th>
+                  </>
+                )}
               </tr>
             </thead>
             <tbody>
